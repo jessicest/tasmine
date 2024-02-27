878363eb47de66e93528383af37d9333655ced7e
Jessica Knight <corence@gmail.com>
2024-02-28 00:22:05 +1100

    great!! each task now has a working checkbox, and, day names are much clearer

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ function dragLeave(event) {
 
 function dragStart(event) {
     event.dataTransfer.setData('text/plain', event.target.id);
+    document.body.classList.add('dragging');
+}
+
+function dragEnd(event) {
+    document.body.classList.remove('dragging');
 }
 
 function drop(event) {
@@ -57,7 +62,7 @@ function makeTask(dateString, i, j) {
 
     var task = document.createElement('div');
     task.classList.add('task');
-    task.innerText = 'task ' + i + ' ' + j;
+    task.innerHTML = '<p>task ' + i + ' ' + j + '<input type="checkbox" /></p>';
 
     taskBox.appendChild(task);
     taskBox.appendChild(makeSlot('task-top'));
@@ -66,6 +71,10 @@ function makeTask(dateString, i, j) {
 }
 
 function init() {
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    document.body.addEventListener('dragend', dragEnd);
+
     var days = document.getElementById('days');
     for (var i = 1; i < 5; ++i) {
         var date = new Date();
@@ -75,11 +84,15 @@ function init() {
         var day = document.createElement('div');
         day.id = 'day ' + dateString;
         day.dataset.date = dateString;
+        day.dataset.year = date.getFullYear();
+        day.dataset.month = date.getMonth() + 1;
+        day.dataset.day = date.getDate();
+        day.dataset.weekday = date.getDay();
         day.classList.add('day');
 
         var title = document.createElement('div');
         title.id = 'title ' + dateString;
-        title.innerText = dateString;
+        title.innerText = daysOfWeek[date.getDay()] + ' ' + date.getDate();
         title.classList.add('title');
         day.appendChild(title);
         day.appendChild(makeSlot('day-top'));
@@ -91,6 +104,14 @@ function init() {
 
         days.appendChild(day);
     }
+}
+
+function pad(character, targetLength, string) {
+    var s = '';
+    for(var i = 0; i < targetLength - string.length; ++i) {
+        s += character;
+    }
+    return s + string;
 }
 
 init();
