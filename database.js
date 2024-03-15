7be66d2a1c2e14ee63763c3cbe9d3a3968859aa8
Jessica Knight <corence@gmail.com>
2024-03-15 11:55:52 +1100

    --- ok let's run an initial trial to iron out some bugs

--- database.js
+++ database.js
@@ -17,13 +17,6 @@ async function initDatabase() {
     });
 }
 
-async function initializeGapiClient() {
-    await gapi.client.init({
-        apiKey: API_KEY,
-        discoveryDocs: [DISCOVERY_DOC],
-    });
-}
-
 function login() {
     tokenClient = google.accounts.oauth2.initTokenClient({
         client_id: CLIENT_ID,
@@ -42,16 +35,6 @@ function login() {
         };
 
         tokenClient.requestAccessToken();
-        /*
-        if (gapi.client.getToken() === null) {
-            // Prompt the user to select a Google Account and ask for consent to share their data
-            // when establishing a new session.
-            tokenClient.requestAccessToken();
-        } else {
-            // Skip display of account chooser and consent dialog for an existing session.
-            tokenClient.requestAccessToken({prompt: ''});
-        }
-        */
     });
 }
 
---
+++ db_tasmine.js
@@ -0,0 +1,53 @@
+
+const INFO = "98j43f9ajfsdofijODFIJSFOEIJFjajfp33983fjdiFSJFDK";
+let passport = localStorage.getItem('passport');
+
+async function initDatabase() {
+}
+
+async function login() {
+    try {
+        const response = await fetch(`https://tasmine.com/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                info: INFO,
+                passport,
+            })
+        });
+        if (!response.ok) {
+            throw "omg lol";
+        }
+        const json = await response.json();
+        console.log(json);
+        localStorage.setItem('passport', json.passport);
+    } catch (error) {
+        console.log(`uh oh! ${error}`);
+    }
+}
+
+async function loadFromDatabase() {
+    try {
+        const response = await fetch(`https://tasmine.com/dump`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                info: INFO,
+                passport,
+            })
+        });
+
+        if (!response.ok) {
+            throw "zomg lol";
+        }
+        const json = await response.json();
+        console.log(json);
+        //return range.values;
+    } catch (error) {
+        console.log(`oh uh! ${error}`);
+    }
+}
